22d84146d5b5a215bcb598181a53b2be4c8acc70
Salvador Leon <salvador.leon1999@gmail.com>
2021-03-01 20:58:00 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -21,102 +21,11 @@ Expected output:
  nap:0
  dog:0
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "smp0_tests.h"
-
-/* Structures */
-typedef struct {
-char *word;
-int counter;
-} WordCountEntry;
-
-int process_stream (WordCountEntry entries[], int entry_count)
-{
-short line_count = 0;
-char buffer[30];
-while (fgets (buffer, sizeof (buffer), stdin)) {
-if (*buffer == '.')
-break;
-
-/* strip '\n' from buffer */
-size_t len = strlen (buffer); /* get word length */
-if (buffer[len - 1] == '\n') /* check for '\n' */
-buffer[--len] = 0; /* overwrite with nul-byte */
-
-/* Compare against each entry */
-if (!strcmp (entries[line_count].word, buffer))
-entries[line_count].counter++;
-if (++line_count == entry_count)
-break;
-}
-return line_count;
-}
-
-void print_result (WordCountEntry entries[], int entry_count)
-{
-fprintf (stdout, "\nResult:\n");
-
-for (int i = 0; i < entry_count; i++) {
-fprintf ("%s:%d\n", entries[i].word, entries[i].counter);
-}
-}
-
-void printHelp (const char *name)
-{
-fprintf (stderr, "usage: %s [-h] [-f FILENAME] <word1> ... <wordN>\n", name);
-}
-
-int main (int argc, char **argv)
-{
-const char *prog_name = *argv;
-/*Add support for matching arbitrary number of words, not just 5.*/
-WordCountEntry entries[argc];
-int entryCount = 0;
-
-for (int i = 1; i < argc; i++) {
-if (*argv[i] == '-') {
-
-switch (argv[1][1]) {
-case 'h':
-		printHelp(prog_name);
-case 'f':
-freopen((*argv)[2],"w",stdout);
-break;
-default:
-fprintf (stderr, "%s: Invalid option %s. Use -h for help.\n",prog_name, *argv);
-}
-}
-else {
-	if (entryCount < LENGTH(entries) - 1) {
-		entries[entryCount].word = *argv + entryCount + 1;
-		entries[entryCount++].counter = 0;
-	}
-}
-argv++;
-}
-}
-
-if (!entryCount) {
-fprintf (stderr,"%s: Please supply at least one word. Use -h for help.\n",prog_name);
-return EXIT_FAILURE;
-}
-
-if (entryCount == 1) {
-fprintf (stdout, "Looking for a single word\n");
-}
-else {
-fprintf (stdout, "Looking for %d words\n", entryCount);
-}
-process_stream (entries, entryCount);
-print_result (entries, entryCount);
-
-return EXIT_SUCCESS;
-fflush(stdout);
-}#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /* Structures */
 typedef struct {
@@ -181,10 +90,10 @@ int main(int argc, char** argv)
 	int entryCount = 0;
 	FILE* Newfile = stdout;
 
-	//if (argc > 1 && !strcmp(argv[1], "-test")) {
-	//	run_smp0_tests(argc - 1, argv + 1);
-	//	return EXIT_SUCCESS;
-	//}
+	if (argc > 1 && !strcmp(argv[1], "-test")) {
+		run_smp0_tests(argc - 1, argv + 1);
+		return EXIT_SUCCESS;
+	}
 
 	entries = (WordCountEntry*)malloc(argc * sizeof(WordCountEntry));
 	argv++;
